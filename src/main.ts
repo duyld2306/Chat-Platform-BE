a504e4569b93b0874e40c2d62948a878bde54e77
Duy <duyld2306@gmail.com>
2022-12-10 11:33:20 +0700

    Implement register user endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const { PORT } = process.env;
-  console.log(PORT)
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
   try {
     await app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
   } catch (err) {
